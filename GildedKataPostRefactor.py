1c55d1651b414b9f0de204162beef0c4fad3b727
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 20:20:56 +0300

    Added Guard caluse for "Backstage passes" Use Guard clause for “Backstage passes …”, to cover the two -> else cases for when the item equals “Backstage passes …”

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -23,10 +23,7 @@ class GildedRose(object):
                     self.item_increase_quality(item)
                 continue
 
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -36,13 +33,18 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
                     item.quality = item.quality - item.quality
+                continue
+
+            if item.quality > 0:
+                    item.quality = item.quality - 1
+            item.sell_in = item.sell_in - 1
+
+            if item.sell_in < 0:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
 
     def item_increase_quality(self, item):
         if item.quality < 50:
