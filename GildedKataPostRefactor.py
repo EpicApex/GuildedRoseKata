aee39cd00d257ee2123f03f50ade9f4fe10309b5
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 21:29:56 +0300

    Added ordinary item class , added conjured item class to have it more readable.

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -24,22 +24,29 @@ class GildedRose(object):
                 continue
             
             elif "Conjured" in item.name:
-                item.quality -= 2
-                item.sell_in -= 1
-                if item.sell_in < 0:
-                    item.quality -= 2
+                self.conjured_item(item)
+                continue
             
             #Our normal items which are not the ones in the Guard Clause
             else:
                 # We made sure in our tests, that item.quality has to be between 0 <= item.quality <= 50 (Taking in consideration that Sulfuras equals 80)
-                if item.quality > 0:
-                        item.quality -= 1
-                
-                item.sell_in -= 1
+                self.ordinary_item(item)
 
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality -=  1
+    def conjured_item(self, item):
+        item.quality -= 2
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality -= 2
+
+    def ordinary_item(self, item):
+        if item.quality > 0:
+                item.quality -= 1
+                
+        item.sell_in -= 1
+
+        if item.sell_in < 0:
+            if item.quality > 0:
+                item.quality -=  1
 
     def aged_brie(self, item):
         self.item_increase_quality(item)
