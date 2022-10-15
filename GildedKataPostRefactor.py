75e6dfa5444657798ceda9eafa55775cdc39f2d5
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 20:35:59 +0300

    #item quality in this if statement can go above 50 and not be sulfurus, but right after that you have an if that takes care of the case where item quality is above 50 nad set it to 50 again.

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -24,21 +24,24 @@ class GildedRose(object):
                 continue
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
+                #item quality in this if statement can go above 50 and not be sulfurus, but right after that you have an if that takes care of the case where item quality is above 50 nad set it to 50 again.
+                item.quality = item.quality + 1
+                if item.sell_in < 11:
                     item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
+
+                if item.quality > 50:
+                    item.quality = 50
+
                 if item.sell_in < 0:
                     item.quality = 0
                 continue
 
             if item.quality > 0:
                     item.quality = item.quality - 1
+            
             item.sell_in = item.sell_in - 1
 
             if item.sell_in < 0:
