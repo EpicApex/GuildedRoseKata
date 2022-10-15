f384fefb365858a3cbf1f6eca149b378018025af
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 20:53:10 +0300

    changed to elif between products of the Inn.
    Also created a new method concert_backstage_pass to have the code more readable

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -15,7 +15,7 @@ class GildedRose(object):
                 item.quality = 80
                 continue
 
-            if item.name == "Aged Brie":
+            elif item.name == "Aged Brie":
                 self.item_increase_quality(item)
                 item.sell_in = item.sell_in - 1
 
@@ -23,30 +23,36 @@ class GildedRose(object):
                     self.item_increase_quality(item)
                 continue
 
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                #item quality in this if statement can go above 50 and not be sulfurus, but right after that you have an if that takes care of the case where item quality is above 50 nad set it to 50 again.
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    item.quality = item.quality + 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.concert_backstage_pass(item)
+                continue
+            
+            #Our normal items which are not the ones in the Guard Clause
+            else:
+                # We made sure in our tests, that item.quality has to be between 0 <= item.quality <= 50 (Taking in consideration that Sulfuras equals 80)
+                if item.quality > 0:
+                        item.quality = item.quality - 1
+                
                 item.sell_in = item.sell_in - 1
 
-                if item.quality > 50:
-                    item.quality = 50
-
                 if item.sell_in < 0:
-                    item.quality = 0
-                continue
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
 
-            if item.quality > 0:
-                    item.quality = item.quality - 1
-            
-            item.sell_in = item.sell_in - 1
+    def concert_backstage_pass(self, item):
+        #item quality in this if statement can go above 50 and not be sulfurus, but right after that you have an if that takes care of the case where item quality is above 50 nad set it to 50 again.
+        if item.sell_in < 6:
+            item.quality = item.quality + 3
+        elif item.sell_in < 11:
+            item.quality = item.quality + 2
+        else:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
 
-            if item.sell_in < 0:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+        if item.quality > 50:
+            item.quality = 50
 
     def item_increase_quality(self, item):
         if item.quality < 50:
