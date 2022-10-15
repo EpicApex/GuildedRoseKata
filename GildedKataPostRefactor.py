43f41d544a09c09e28d5137e347581ce7e5e03fd
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 20:00:44 +0300

    Use guard clause for “Aged Brie” to cover the two -> else cases for when the item equals “Aged Brie”

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -15,9 +15,18 @@ class GildedRose(object):
                 item.quality = 80
                 continue
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                item.sell_in = item.sell_in - 1
+
+                if item.sell_in < 0:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                continue
+
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
@@ -31,16 +40,11 @@ class GildedRose(object):
                                 item.quality = item.quality + 1
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality - item.quality
 
 
 class Item:
