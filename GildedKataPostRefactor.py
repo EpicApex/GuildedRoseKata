bf91fa9eea44d538720c1ecc3a883108461462a7
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 20:28:03 +0300

    item.quality - item.quality = 0

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -34,7 +34,7 @@ class GildedRose(object):
                             item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    item.quality = item.quality - item.quality
+                    item.quality = 0
                 continue
 
             if item.quality > 0:
