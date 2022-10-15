8c2226658e7623c2f1e01bd5934cf54fc1021ccb
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 21:20:04 +0300

    Added handling guard clause for handling conjured items

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -23,21 +23,27 @@ class GildedRose(object):
                 self.concert_backstage_pass(item)
                 continue
             
+            elif "Conjured" in item.name:
+                item.quality -= 2
+                item.sell_in -= 1
+                if item.sell_in < 0:
+                    item.quality -= 2
+            
             #Our normal items which are not the ones in the Guard Clause
             else:
                 # We made sure in our tests, that item.quality has to be between 0 <= item.quality <= 50 (Taking in consideration that Sulfuras equals 80)
                 if item.quality > 0:
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                 
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
 
                 if item.sell_in < 0:
                     if item.quality > 0:
-                        item.quality = item.quality - 1
+                        item.quality -=  1
 
     def aged_brie(self, item):
         self.item_increase_quality(item)
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
 
         if item.sell_in < 0:
             self.item_increase_quality(item)
@@ -45,11 +51,11 @@ class GildedRose(object):
     def concert_backstage_pass(self, item):
         #item quality in this if statement can go above 50 and not be sulfurus, but right after that you have an if that takes care of the case where item quality is above 50 nad set it to 50 again.
         if item.sell_in < 6:
-            item.quality = item.quality + 3
+            item.quality += 3
         elif item.sell_in < 11:
-            item.quality = item.quality + 2
+            item.quality += 2
         else:
-            item.quality = item.quality + 1
+            item.quality += 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             item.quality = 0
@@ -59,7 +65,7 @@ class GildedRose(object):
 
     def item_increase_quality(self, item):
         if item.quality < 50:
-            item.quality = item.quality + 1
+            item.quality += 1
 
 
 class Item:
