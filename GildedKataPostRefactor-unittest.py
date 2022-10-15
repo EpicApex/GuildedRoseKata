51fcd5384eaa0ed211be4572b4868cf82f29847e
Omri Freidenberg <omri.f@uveye.com>
2022-10-15 21:18:49 +0300

    Added unit tests for conjured items ->> Conjured items Degrade twice as fast in quality  AND conjured_items_sell_in_0_quality (instead of degrading in 2 now it degrades in 4 ) !!

--- GildedKataPostRefactor-unittest.py
+++ GildedKataPostRefactor-unittest.py
@@ -37,8 +37,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         # self.assertEqual(50 , items[0].quality)
-        print ("quality ="+str(items[0].quality))
-        print ("sell_in_days ="+str(items[0].sell_in))
+        self.print_sell_in_and_quality(items)
 
     #Quality of normal item degrades upon time passed.
     def test_normal_item_quality_degrade(self):
@@ -67,56 +66,47 @@ class GildedRoseTest(unittest.TestCase):
     def test_normal_item_quality_between_0_and_50(self):
         items = [Item("Bow Of Peril", 10, 0)]
         self.assertTrue(items[0].quality >= 0 and items[0].quality <= 50)
-        print ("########################################################")
+        self.print_quality_and_seperator(items)
         print ("#### Quality of an item cant be negative or above 50")
-        print ("quality ="+str(items[0].quality))
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        print ("quality ="+str(items[0].quality))
-        print ("sell_in_days ="+str(items[0].sell_in))
+        self.print_sell_in_and_quality(items)
 
     #Quality of backstage pass increases over time passed above 10 days.
     def test_backstage_pass_quality_increases(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 20, 45)]
-        print ("########################################################")
-        print ("quality ="+str(items[0].quality))
+        self.print_quality_and_seperator(items)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(19, items[0].sell_in)
         self.assertEqual(46, items[0].quality)
-        print ("quality ="+str(items[0].quality))
-        print ("sell_in_days ="+str(items[0].sell_in))
+        self.print_sell_in_and_quality(items)
 
     #Quality of backstage pass increases by 2 over time passed between 10 days and 6 days
     def test_backstage_pass_quality_increases_by_2_between_10_and_6_days_left(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 6, 45)]
         self.assertTrue(items[0].sell_in > 5 and items[0].sell_in <= 10)
-        print ("########################################################")
-        print ("quality ="+str(items[0].quality))
+        self.print_quality_and_seperator(items)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(5, items[0].sell_in)
         self.assertEqual(47, items[0].quality)
-        print ("quality ="+str(items[0].quality))
-        print ("sell_in_days ="+str(items[0].sell_in))
+        self.print_sell_in_and_quality(items)
 
     #Quality of backstage pass increases by 3 over time passed between 5 days and 0 days
     def test_backstage_pass_quality_increases_by_3_between_5_and_0_days_left(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
         self.assertTrue(items[0].sell_in > 0 and items[0].sell_in <= 5)
-        print ("########################################################")
-        print ("quality ="+str(items[0].quality))
+        self.print_quality_and_seperator(items)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(4, items[0].sell_in)
         self.assertEqual(48, items[0].quality)
-        print ("quality ="+str(items[0].quality))
-        print ("sell_in_days ="+str(items[0].sell_in))
+        self.print_sell_in_and_quality(items)
 
     #Quality of backstage pass when less then 0 days left => quality = 0
     def test_backstage_pass_quality_less_than_0_days_left(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 45)]
-        print ("########################################################")
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
@@ -124,13 +114,36 @@ class GildedRoseTest(unittest.TestCase):
     #Quality of backstage pass cant go above 50
     def test_backstage_pass_quality_cant_go_above_50(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 1, 50)]
-        print ("########################################################")
-        print ("quality ="+str(items[0].quality))
+        self.print_quality_and_seperator(items)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(50, items[0].quality)
+        self.print_sell_in_and_quality(items)
+
+    #Test the new conjured items which will be degrading twice as fast
+    def test_conjured_items_quality_degrade_twice_as_fast(self):
+        items = [Item("Conjured Unholy Flail +10", 15, 40)]
+        self.print_quality_and_seperator(items)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(38, items[0].quality)
+        self.print_sell_in_and_quality(items)
+
+    def test_conjured_items_sell_in_0_quality(self):
+        items = [Item("Conjured Unholy Flail +10", 0, 40)]
+        self.print_quality_and_seperator(items)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(36, items[0].quality)
+        self.print_sell_in_and_quality(items)
+
+    def print_sell_in_and_quality(self, items):
         print ("quality ="+str(items[0].quality))
         print ("sell_in_days ="+str(items[0].sell_in))
+
+    def print_quality_and_seperator(self, items):
+        print ("########################################################")
+        print ("quality ="+str(items[0].quality))
         
 if __name__ == '__main__':
     unittest.main()
