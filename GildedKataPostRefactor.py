380907b301e6b2593ff8f1942a7555c64dc22a2d
Omri Freidenberg <omri.f@uveye.com>
2022-10-17 19:20:33 +0300

    blarg missed one.

--- GildedKataPostRefactor.py
+++ GildedKataPostRefactor.py
@@ -67,7 +67,7 @@ class GildedRose(object):
             item.quality += 2
         else:
             item.quality += 1
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
         if item.sell_in < 0:
             item.quality = 0
 
